a2042545654e9120b87126d795424b4f3d88a853
Jan Bentzen <hisnamewasjaan@gmail.com>
2021-12-27 13:25:51 +0100

    worker supervisor

---
+++ lib/pooly/worker_supervisor.ex
@@ -0,0 +1,30 @@
+defmodule Pooly.WorkerSupervisor do
+  use Supervisor
+
+  def start_link({_, _, _} = mod_fun_args) do
+    Supervisor.start_link(__MODULE__, mod_fun_args)
+  end
+
+  #############
+  # Callbacks #
+  #############
+
+  def init({mod, fun, args}) do
+    worker_opts = [
+      restart: :permanent,
+      function: fun
+    ]
+
+    # create child specification
+    children = [Supervisor.Spec.worker(mod, args, worker_opts)]
+    opts = [
+      strategy: :simple_one_for_one,
+      max_restarts: 5,
+      max_seconds: 5
+    ]
+
+    # create supervisor specification
+    Supervisor.Spec.supervise(children, opts)
+  end
+
+end
